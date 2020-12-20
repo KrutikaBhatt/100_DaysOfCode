9a5ef7268a9c21039131bdfa24323df2074ee9c5
KrutikaBhatt <65107474+KrutikaBhatt@users.noreply.github.com>
2020-12-20 10:11:36 +0530

    Update Day11.java

--- Day11.java
+++ Day11.java
@@ -4,13 +4,18 @@ import java.util.LinkedList;
 // Write an algorithm that computes the reversal of a directed graph.
 // For example, if a graph consists of A -> B -> C, it should become A <- B <- C.
 
+// For this question the Vertices are selected to be Integers.
+
 public class Day11 {
 	
 	public  static class Graph{
+		// Created a class Graph with fixed vertices and the Linked List for edges 
 		int vertice;
 		LinkedList<Integer>[] adjlist;
 		
+		// Constructor
 		Graph(int vertice){
+			
 			this.vertice = vertice;
 			adjlist = new LinkedList[vertice];
 			
@@ -20,19 +25,23 @@ public class Day11 {
 			}
 		}
 		
-		public void addedge(int source ,int dest) {
-			adjlist[source].addFirst(dest);
+		// Add Edges
+		public void addedge(int source ,int destination) {
+			
+			adjlist[source].addFirst(destination);
 		}
 		
+		// Method to reverse a Graph
 		public Graph reverse(Graph graph) {
-			
 			Graph reversegraph = new Graph(vertice);
 			// Inputs a graph and returns a reversed Graph
+			
 			for(int i=0;i<vertice;i++) {
 				LinkedList<Integer> nodelist =adjlist[i];
 				
 				int source =i;
 				for(int j=0;j<nodelist.size();j++) {
+		
 					int destination = nodelist.get(j);
 					reversegraph.addedge(destination,source );
 					
@@ -41,10 +50,13 @@ public class Day11 {
 			return reversegraph;
 		}
 		
+		
 		public void printgraph() {
+			
 			for(int i=0;i<vertice;i++) {
 				System.out.print(i);
 				LinkedList<Integer> nodelist = adjlist[i];
+				
 				for (int j = 0; j <nodelist.size() ; j++) {
                     System.out.print("->" + nodelist.get(j));
                 }
